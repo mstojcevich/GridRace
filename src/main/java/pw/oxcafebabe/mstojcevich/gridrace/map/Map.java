28233eae6a3fb3f19c484d4d66f30d37ca7b2b81
Marcus Stojcevich <mstojcevich@gmail.com>
2014-09-21 15:53:25 -0400

    Add basic map reading. Untested. Pukeworthy.

---
+++ src/main/java/pw/oxcafebabe/mstojcevich/gridrace/map/Map.java
@@ -0,0 +1,34 @@
+package pw.oxcafebabe.mstojcevich.gridrace.map;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by marcusant on 9/21/2014.
+ */
+public class Map {
+
+    private final boolean[][] blockValidity;
+
+    public Map(InputStream mapFileStream) throws IOException {
+        BufferedReader mapReader = new BufferedReader(new InputStreamReader(mapFileStream));
+        List<Boolean[]> validityList = new ArrayList<Boolean[]>();
+        String currentLine;
+        while((currentLine = mapReader.readLine()) != null) {
+            List currentLineValidity = new ArrayList<Boolean>();
+            for(char c : currentLine.toCharArray()) {
+                currentLineValidity.add(c == 1 ? true : false);
+            }
+            validityList.add((Boolean[])currentLineValidity.toArray());
+        }
+        blockValidity = validityList.toArray(new boolean[][]{});
+    }
+
+    public boolean isBlockValid(int x, int y) {
+        return blockValidity[y][x];
+    }
+
+}
